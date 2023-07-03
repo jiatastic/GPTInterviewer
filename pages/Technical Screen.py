02013f9c2159b343b14a90b9f1aec45e6218e395
jiatastic <hjia4@fordham.edu>
2023-07-03 03:37:09 -0400

    prompt update

--- pages/Technical Screen.py
+++ pages/Technical Screen.py
@@ -107,7 +107,7 @@ def initialize_session_state():
             input_variables=["history", "input"],
             template="""I want you to act as an interviewer strictly following the guideline in the current conversation.
                             
-                            Ask me questions and wait for my answers.
+                            Ask me questions and wait for my answers like a real person.
                             Do not write explanations.
                             Ask question like a real person, only one question at a time.
                             Do not ask the same question.
